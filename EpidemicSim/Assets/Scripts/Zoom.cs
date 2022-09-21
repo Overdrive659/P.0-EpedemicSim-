fcffccbdc6af7a77ac3909a7e2fa4fe0e76119ac
Overdrive659 <oskar.olars@gmail.com>
2022-09-21 15:36:23 +0200

    Camera Zoom Script
    Adds script to Main Camera which changes orthographic size with scroll.

---
+++ EpidemicSim/Assets/Scripts/Zoom.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    private float ScrollSpeed = 10;
+    private Camera ZoomCamera;
+
+
+
+    void Start()
+    {
+        ZoomCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ZoomCamera.orthographic) 
+        {
+            ZoomCamera.orthographicSize -= Input.GetAxisRaw("Mouse ScrollWheel") * ScrollSpeed;
+        }
+    }
+}
