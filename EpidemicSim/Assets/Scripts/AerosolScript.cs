d84408d8df76a86671e871cfe3f01caad049b803
Overdrive659 <61062429+Overdrive659@users.noreply.github.com>
2022-11-30 15:43:08 +0100

    NEED COROUTINES

--- EpidemicSim/Assets/Scripts/AerosolScript.cs
+++ EpidemicSim/Assets/Scripts/AerosolScript.cs
@@ -7,19 +7,48 @@ using UnityEngine;
 public class AerosolScript : MonoBehaviour
 {
     public float timer = 0.0f;
+    public float cloudDensity = 100;
+    public int endTime;
+    float startingSize;
+    public float timeStep;
+
     // Start is called before the first frame update
     void Start()
     {
+        
+        endTime = UnityEngine.Random.Range(9, 15);
+        startingSize = UnityEngine.Random.Range(3f, 6f);
+        transform.localScale = new Vector3(startingSize, startingSize, startingSize);
+        //float densityDecrease = cloudDensity / (endTime - startingSize);
+        timeStep = (endTime / cloudDensity);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-        int seconds = Convert.ToInt32(timer % 60);
-        Debug.Log(seconds);
+        timer += Time.deltaTime * 0.3f;
+        //int seconds = Convert.ToInt32(timer % 60);
+        Debug.Log(timeStep);
+        if (timer % timeStep == 0)
+        { 
+            Debug.Log("jag är inne");
+            cloudDensity = cloudDensity - 1;
+        }
 
-        transform.localScale = new Vector3(seconds, seconds, seconds);
+        if (timer > transform.localScale.x)
+        {
+            transform.localScale = new Vector3(timer, timer, timer);
+        }
+
+        if (timer > endTime)
+        {
+            Destroy(gameObject);
+        }
+
+        
+        
+         
+        
     }
 }
